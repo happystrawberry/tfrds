094ff66033fbd370cfee0edb03f3014f758966c8
happystrawberry <inforhub@hotmail.com>
2021-08-27 01:22:48 +0000

    tfpgsql

---
+++ rds00/modules/rds/variables.tf
@@ -0,0 +1,12 @@
+
+variable region00 {
+  type        = string
+  default     = "ap-southeast-2"
+  description = "default region"
+}
+
+variable mycredential {
+  type        = string
+  default     = "/Users/stephenwu/.aws/credentials"
+  description = "my credentials"
+}
