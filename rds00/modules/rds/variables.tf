f87631596ca77e6d44654af28b63babfec056851
happystrawberry <inforhub@hotmail.com>
2021-08-27 18:02:05 +1000

    latest branch commit

---
+++ rds00/modules/rds/variables.tf
@@ -0,0 +1,12 @@
+
+variable region00 {
+  type        = string
+  default     = "ap-southeast-2"
+  description = "default region"
+}
+
+variable mycredential {
+  type        = string
+  default     = ""
+  description = "my credentials"
+}
