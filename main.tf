a19fc4deaee553f8b6e5fc0e3373bf41e5f9b749
happystrawberry <inforhub@hotmail.com>
2021-08-28 19:08:07 +1000

    rm resource

--- main.tf
+++ main.tf
@@ -9,10 +9,3 @@ terraform {
     }
   }
 }
-
-     # An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
-}
