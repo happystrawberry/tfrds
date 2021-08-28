70de757c57fd3b7da11686c70806e94c3086d1d4
happystrawberry <inforhub@hotmail.com>
2021-08-28 19:18:15 +1000

    modified backend

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 terraform {
   backend "remote" {
-         # The name of your Terraform Cloud organization.
-    organization = "example-organization"
+    organization = "sw-org"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-      name = "example-workspace"
+      name = "git-tf"
     }
+  }
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
   }
 }
