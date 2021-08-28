ff93f48f6668e3099f93e02e30c74d93ee57cecc
happystrawberry <inforhub@hotmail.com>
2021-08-28 19:47:03 +1000

    local echo

--- main.tf
+++ main.tf
@@ -12,4 +12,7 @@ resource "null_resource" "example" {
   triggers = {
     value = "A example resource that does nothing!"
   }
+  provisioner "local-exec" {
+    command = "echo 'call out from provisioner local-exec' "
+  }
 }
